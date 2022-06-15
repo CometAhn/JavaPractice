d4b408fc298fcfac1f8cd3b90dd2d2c45287aa6e
home <home@home-PC>
2022-06-15 15:36:09 +0900

    update

--- JavaProject/src/Do_While.java
+++ JavaProject/src/Do_While.java
@@ -4,6 +4,8 @@ public class Do_While {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// 적어도 한 줄은 출려되는 문장
+		/*	
 		int i = 10;
 		
 		do {
@@ -13,6 +15,23 @@ public class Do_While {
 		
 		} while(i>100);
 		//} while(i<15);
+		*/
+		
+		//do~while문을 이용하여 구구단 만들기
+		int row = 1;
+		int column = 2;
+		
+		do {
+			row++;
+			do {
+			 System.out.printf("%3d*%d = %2d,",row, column,row*column);
+				column++;
+			 
+			} while(column<10);
+			column = 2;
+			System.out.printf("\n");
+			
+		}while(row<10);
 		
 	}
 
