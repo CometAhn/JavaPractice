fc52303523305e276d8214ad668f528fa0d04bd8
home <home@home-PC>
2022-06-15 16:10:48 +0900

    update

---
+++ JavaProject/src/Prca.java
@@ -0,0 +1,15 @@
+
+public class Prca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 3;
+		a = 4;
+		
+		final double PI = 3.14;
+		//PI = 3.15; // 에러 발생
+		
+	}
+
+}
