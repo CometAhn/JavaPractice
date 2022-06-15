bda9683107830b6631bf4662955ffb68ffa8dca7
home <home@home-PC>
2022-06-15 15:05:53 +0900

    update

---
+++ JavaProject/src/Do_While.java
@@ -0,0 +1,18 @@
+
+public class Do_While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+		
+		do {
+			
+			System.out.println("적어도 한 번은 출력되는 문장");
+			i++;
+			
+		} while(i<15);
+		
+	}
+
+}
