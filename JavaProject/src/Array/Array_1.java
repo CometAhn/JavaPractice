9776fda109ae865748d541221d6882731f00f322
home <home@home-PC>
2022-06-16 12:39:04 +0900

    update

--- JavaProject/src/Array/Array_1.java
+++ JavaProject/src/Array/Array_1.java
@@ -16,7 +16,7 @@ public class Array_1 {
 		
 		System.out.println("현재 첫 번째 요소의 값 = "+ student[2]);
 		*/
-		
+	/*	
 		//길이가5인 배열 생성, 배열 길이 생략
 		int[] a = new int[] {10,20,4,25,18};
 		
@@ -36,6 +36,18 @@ public class Array_1 {
 
 		System.out.println("");
 		System.out.println(Arrays.toString(c));
+		*/
+		
+		//시험점수 평균 구하기
+		int[] student = {100, 95, 90, 88, 93};
+		int sum = 0;
+		for(int i=0;i<student.length;i++) {
+			sum += student[i];
+		}
+		
+		System.out.println("시험점수의 평균 : " + sum/(float)student.length);
+			
+		
 	}
 
 }
