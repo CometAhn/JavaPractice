aff2092a4fc20210047ce0841bdfc2a1d965421e
home <home@home-PC>
2022-06-16 15:34:51 +0900

    update

--- JavaProject/src/Loop/RandomNumberGame.java
+++ JavaProject/src/Loop/RandomNumberGame.java
@@ -1,95 +1,10 @@
 package Loop;
 import java.util.Scanner;
 
-public class Break {
+public class RandomNumberGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		// break문 활용,(조건문 탈출)
-		/*
-		 * int i=1; while(i<=10) {
-		 * 
-		 * if(i == 8) {
-		 * 
-		 * break;
-		 * 
-		 * } System.out.printf("%2d,",i++);
-		 * 
-		 * }
-		 */
-		// 중단하고자 하는 숫자 입력 : n에서 충력이 중단되었습니다.
-		/*
-		 * Scanner input = new Scanner(System.in); int num = 0;
-		 * 
-		 * System.out.printf("중단 하고자 하는 숫자 입력 : ");
-		 * 
-		 * int i = input.nextInt();
-		 * 
-		 * while(true) {
-		 * 
-		 * System.out.println(num); num++;
-		 * 
-		 * if(num==i)
-		 * 
-		 * break; } System.out.printf("%d에서 중단되었습니다.",num);
-		 */
-		/*
-		 * Scanner sc = new Scanner(System.in); int sum = 0; int i;
-		 * 
-		 * while(true) { System.out.println("더할 숫자를 입력하세요 : (종료하려면 0입력)");
-		 * 
-		 * i = sc.nextInt(); if(i==0) { break; }
-		 * 
-		 * sum+= i;
-		 * 
-		 * } System.out.println("현재까지 총합 = " + sum);
-		 */
-
-		// 점수를 계속 입력 받다가 0이 입력되면 입력된 수의 개수, 합계, 평균을 출력하는 프로그램을 작성하시오.(p.67 참고)
-		/*
-		 * Scanner sc = new Scanner(System.in); int sum = 0; int i; int count =
-		 * 0;
-		 * 
-		 * while(true) { System.out.println("더할 숫자를 입력하세요 : (종료하려면 0입력)");
-		 * 
-		 * i = sc.nextInt(); if(i==0) { break; }
-		 * 
-		 * sum+= i; count++;
-		 * 
-		 * } //System.out.println("현재까지 총합 = " + sum);
-		 * System.out.println("입력된 수의 개수 = " + count);
-		 * System.out.println("입력된 수의 합계 = " + sum);
-		 * System.out.println("입력된 수의 평균 = " + sum/count);
-		 */
-		// 위 문제를 참고하여 정수를 계속 입력 받다가 0이 입력되면 입력된 수들 중에서 홀수의 합계, 홀수의 평균을 출력하는
-		// 프로그램을 작성하시오
-		// 입력 데이터 5, 8, 17, 6, 31, 0
-		// 출력
-		// 홀수의 합 = 53
-		// 홀수의 평균 = 17
-		/*
-		 * Scanner sc = new Scanner(System.in); int sum = 0; int i; int count =
-		 * 0; int odd = 0; int oddsum = 0; int oddcount = 0;
-		 * 
-		 * while(true) { System.out.println("더할 숫자를 입력하세요 : (종료하려면 0입력)");
-		 * 
-		 * 
-		 * i = sc.nextInt();
-		 * 
-		 * if(i==0) { break; }
-		 * 
-		 * if(i%2==1) { odd++; oddsum+= i; oddcount++; }
-		 * 
-		 * sum+= i; count++;
-		 * 
-		 * } if(odd==0) { //System.out.println("홀수가 입력되지 않았습니다.");
-		 * System.out.println("현재까지 총합 = " + sum); } else {
-		 * 
-		 * System.out.println("입력된 홀수의 개수 = " + oddcount);
-		 * System.out.println("입력된 홀수의 합계 = " + oddsum);
-		 * System.out.println("입력된 수의 평균 = " + oddsum/oddcount); }
-		 */
 		// 숫자 맞추기 게임.
 		// 랜덤한 숫자(random) 생성. 랜덤 : 72; 몰?루
 		// ㄴ Math.random() : 0~1 구간에서 소수점의 난수를 생성.
@@ -194,4 +109,5 @@ public class Break {
 			}
 		} // while 종료
 	}
+
 }
--- JavaProject/src/Pointer/Array_2.java
+++ JavaProject/src/Pointer/Array_2.java
@@ -1,74 +1,63 @@
 package Pointer;
-import java.util.*;
+import java.util.Scanner;
 
 public class Array_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	/*	//시험점수 평균 구하기
-		int[] student = {100, 95, 90, 88, 93};
-		int sum = 0;
-		for(int i=0;i<student.length;i++) {
-			sum += student[i];
-		}
-		
-		System.out.println("시험점수의 평균 : " + sum/(float)student.length);
-	*/	
-		
+		/*
+		 * //시험점수 평균 구하기 int[] student = {100, 95, 90, 88, 93}; int sum = 0;
+		 * for(int i=0;i<student.length;i++) { sum += student[i]; }
+		 * 
+		 * System.out.println("시험점수의 평균 : " + sum/(float)student.length);
+		 */
+
 		// 배열 이용. 배열 길이가 5(int scores)
 		// 사용자가 5번 입력한 숫자의 합과 평균을 구하기
-/*		Scanner in = new Scanner(System.in);
+		/*
+		 * Scanner in = new Scanner(System.in);
+		 * 
+		 * 
+		 * int[] scores = new int[5]; int sum = 0; int i;
+		 * 
+		 * for(i=0;i<scores.length;i++) {
+		 * 
+		 * System.out.printf("입력할 값"); scores[i] = in.nextInt(); sum+=
+		 * scores[i]; }
+		 * 
+		 * System.out.printf("\n합계 : %d\n",sum);
+		 * 
+		 * System.out.printf("평균 : %s",sum/(float)scores.length);
+		 */
 
-		
-		int[] scores = new int[5];
-		int sum = 0;
-		int i;
-		
-		for(i=0;i<scores.length;i++) {
-
-			System.out.printf("입력할 값");
-			scores[i] = in.nextInt();
-			sum+= scores[i];
-		}
-
-		System.out.printf("\n합계 : %d\n",sum);
-	
-		System.out.printf("평균 : %s",sum/(float)scores.length);
-*/
-		
 		// 1. 학생수를 입력해서 학생 수 만큼 성적을 입력하여 학생들의 총합과 학생들의 평균값을 출력하시오.
 		// 2. 성적 입력시 0미만이거나 100초과인 경우 데이터를 다시 입력 받을 수 있게 처리하시오.
-		// 학생수 배열
-		// 점수 배열
 		Scanner input = new Scanner(System.in); // 스캐너 생성
-		
+
 		int stunum = 0; // 학생수 배열 수 만들기용
-		
+
 		System.out.println("학생수는 몇 명입니까?");
-		stunum = input.nextInt();	
-		
+		stunum = input.nextInt();
+
 		int[] scores = new int[stunum]; // 점수들 수
 		int sum = 0; // 합계
-		int min=0; // 최소값
-		int max=100; // 최대값
-		
-		for(int i=0;i<stunum;i++) {
-			System.out.println("점수는 몇점입니까??");	
+		int min = 0; // 최소값
+		int max = 100; // 최대값
+
+		for (int i = 0; i < stunum; i++) {
+			System.out.println("점수는 몇점입니까??");
 			scores[i] = input.nextInt(); // 각 학생의 점수.
-			
-			if((min>scores[i])||(max<scores[i])) {
+
+			if ((min > scores[i]) || (max < scores[i])) {
 				System.out.printf("데이터가 너무 크거나 작습니다. 다시 입력하세요\n");
 				i--; // 카운트 다시 1 빼기.
 				continue;
-				}
-			else {
+			} else {
 				sum += scores[i]; // sum = 점수 총합.
-				
 			}
 		}
-		System.out.printf("점수 총합 :  %d\n",sum);
-		System.out.printf("점수 평균 :  %.1f",(float)sum/stunum);
-			
+		System.out.printf("점수 총합 :  %d\n", sum);
+		System.out.printf("점수 평균 :  %.1f", (float) sum / stunum);
 	}
 
 }
