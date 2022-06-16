81bffd4a89021ed2413e62106a75ce7123545610
home <home@home-PC>
2022-06-16 12:44:35 +0900

    update

---
+++ JavaProject/src/Array/Array_2.java
@@ -0,0 +1,30 @@
+package Array;
+import java.util.*;
+
+public class Array_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//시험점수 평균 구하기
+		int[] student = {100, 95, 90, 88, 93};
+		int sum = 0;
+		for(int i=0;i<student.length;i++) {
+			sum += student[i];
+		}
+		
+		System.out.println("시험점수의 평균 : " + sum/(float)student.length);
+		
+		
+		// 배열 이용, 배열 길이 5, 입력 5, 입력된 값의 평균
+		Scanner in = new Scanner(System.in);
+		int scores[] = new int[5];
+		int sum =0;
+		
+		for(int i;i<scores.length;i++) {
+			
+			
+		}
+			
+	}
+
+}
