b1e0ecfda7f82b642fc47b8207054cd122dd88e4
home <home@home-PC>
2022-06-16 16:23:04 +0900

    update

---
+++ JavaProject/src/Pointer/Array_5_Foreach.java
@@ -0,0 +1,41 @@
+package Pointer;
+
+public class Array_5_Foreach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// string
+		String[] numbers = {"one", "two", "three"};
+
+		for (int i = 0; i < numbers.length; i++) { // ±âÁ¸
+			// System.out.println(numbers[i]);
+		}
+
+		for (String i : numbers) { // foreach
+			// System.out.println(i);
+		}
+
+		// int
+		int[] number = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		for (int j : number) {
+			// System.out.println(j);
+		}
+
+		// È¦¼ö¸¸
+		int[] numberodd = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		for (int j : numberodd) {
+			if (j % 2 == 1) {
+				// System.out.println(j);
+			}
+		}
+
+		// Â¦¼ö¸¸
+		int[] numbereven = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+		for (int j : numbereven) {
+			if (j % 2 == 0) {
+				System.out.println(j);
+			}
+		}
+	}
+
+}
