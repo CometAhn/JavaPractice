61f4bf74352fae01637de17fe7f1b3d15c271334
home <home@home-PC>
2022-06-16 12:00:43 +0900

    update

---
+++ JavaProject/src/배열/Array_1.java
@@ -0,0 +1,19 @@
+package 배열;
+
+public class Array_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] student = new int[3];
+		System.out.println("현재 자동으로 초기화 된 값:"+ student[0]);
+		
+		student[0] = 30;
+		student[1] = 20;
+		student[2] = 10;
+		
+		System.out.println("현재 첫 번째 요소의 값 = "+ student[2]);
+		
+	}
+
+}
