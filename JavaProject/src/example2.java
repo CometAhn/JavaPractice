0882f261c85935e00256fbfc43604b8780251fbd
home <home@home-PC>
2022-06-22 14:43:51 +0900

    update

--- JavaProject/src/example2.java
+++ JavaProject/src/example2.java
@@ -1,23 +1,25 @@
 
 public class example2 {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    int i = 0;
-    System.out.println("첫번쩨 i = " + i);
-    System.out.println("두번째 i = " + i++);
-    System.out.println("서번째 i = " + i);
+		int i = 0;
+		System.out.println("첫번쩨 i = " + i);
+		System.out.println("두번째 i = " + i++);
+		System.out.println("서번째 i = " + i);
 
-    i = 0;
-    System.out.println("네번째 i = " + ++i);
-    System.out.println("다셋번째 i = " + i);
+		i = 0;
+		System.out.println("네번째 i = " + ++i);
+		System.out.println("다셋번째 i = " + i);
 
-    int j = 10;
-    System.out.println("첫번쩨 j = " + j);
-    System.out.println("두번쩨 j = " + j--);
-    System.out.println("세번쩨 j = " + j);
+		int j = 10;
+		System.out.println("첫번쩨 j = " + j);
+		System.out.println("두번쩨 j = " + j--);
+		System.out.println("세번쩨 j = " + j);
 
-
-  }
+		j = 10;
+		System.out.println("네번쩨 j = " + --j);
+		System.out.println("다섯번쩨 j = " + j);
+	}
 
 }
