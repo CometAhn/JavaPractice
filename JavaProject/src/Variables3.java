059727d99306a0b8a3bad253925e7109a21d2f05
home <home@home-PC>
2022-06-22 11:46:13 +0900

    update

---
+++ JavaProject/src/Variables3.java
@@ -0,0 +1,17 @@
+
+public class Variables3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int byteval = 10;
+		int intval = byteval;
+
+		intval = 20;
+		double doubleval = intval;
+		System.out.println(intval);
+		System.out.println(doubleval);
+
+	}
+
+}
