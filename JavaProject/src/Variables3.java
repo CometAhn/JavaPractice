0a15c88656ff13b5bb7361d1d8453d34b0646f48
home <home@home-PC>
2022-06-22 12:00:24 +0900

    update

--- JavaProject/src/Variables3.java
+++ JavaProject/src/Variables3.java
@@ -4,7 +4,7 @@ public class Variables3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int byteval = 10;
+		byte byteval = 10;
 		int intval = byteval;
 
 		intval = 20;
@@ -12,6 +12,13 @@ public class Variables3 {
 		System.out.println(intval);
 		System.out.println(doubleval);
 
+		char charval = 'A';
+		intval = charval;
+		byteval = 65;
+		charval = (char) byteval; // 음수가 지정될 수 있는 byte, int등의 타입은
+									// char타입으로 자동타입변환 할 수 없다.
+
+		System.out.println(charval); // 65, A
 	}
 
 }
