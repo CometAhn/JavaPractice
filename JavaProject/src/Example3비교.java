986312cfb815cfbcf862113a6ad9fff8e6b09588
CometAhn <comet@ipi.pw>
2022-06-23 11:47:21 +0900

    update

--- JavaProject/src/Example3비교.java
+++ JavaProject/src/Example3비교.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Example3비교 {
 
@@ -39,15 +40,46 @@ public class Example3비교 {
 		 * System.out.println("거짓 or 참일 때 : " + (e || d));
 		 * System.out.println("거짓 or 거짓일 때 : " + (e || e));
 		 */
+		/*
+		 * int x = 10; int y = 15; // AND 둘 다 만족하는지 System.out.println(x > 5 && x < 25);
+		 * // true System.out.println(x > 8 && x < 10); // false // OR 하나 이상 만족하는지
+		 * System.out.println(x > 15 || x < 25); // true System.out.println(x > 8 || x <
+		 * 10); // true
+		 */
+		/*
+		 * int num = 0; int pay = 0; Scanner input = new Scanner(System.in);
+		 * 
+		 * System.out.println("인원은 몇명?"); num = input.nextInt();
+		 * System.out.println("주문 금액은?"); pay = input.nextInt();
+		 * 
+		 * if (num >= 2 && pay >= 10000) {
+		 * 
+		 * System.out.println("예악 받는다"); } else {
+		 * 
+		 * System.out.println("예악 안 받는다"); }
+		 * 
+		 * int money = 0;
+		 * 
+		 * if (money >= 5000 && money < 10000) { System.out.println("사원 목록 본다");
+		 * 
+		 * }
+		 */
+		int goal = 0; // 3000
+		int plus = 0; // 750
+		Scanner input = new Scanner(System.in);
 
-		int x = 10;
-		int y = 15;
-		// AND 둘 다 만족하는지
-		System.out.println(x > 5 && x < 25); // true
-		System.out.println(x > 8 && x < 10); // false
-		// OR 하나 이상 만족하는지
-		System.out.println(x > 15 || x < 25); // true
-		System.out.println(x > 8 || x < 10); // true
+		System.out.println("판매량은?");
+		goal = input.nextInt();
+		System.out.println("이익금은?");
+		plus = input.nextInt();
+
+		float plusper = ((float) plus / goal) * 100;
+		// System.out.printf("%.2f", plusper);
+		if (goal >= 3000 && plusper >= 20) {
+			System.out.println("보너스 지급?");
+
+		}
+
 	}
 
 }
