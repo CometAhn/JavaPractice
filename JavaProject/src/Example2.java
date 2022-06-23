944e3939a802ea51ba09ef6d8e0bd1ca2192bd59
CometAhn <comet@ipi.pw>
2022-06-23 09:45:45 +0900

    update

--- JavaProject/src/Example2.java
+++ JavaProject/src/Example2.java
@@ -1,5 +1,5 @@
 
-public class example2 {
+public class Example2 {
 
 	public static void main(String[] args) {
 		/*
---
+++ JavaProject/src/Example3.java
@@ -0,0 +1,17 @@
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 20;
+		int c = 30;
+
+		System.out.println(a < b); // true
+		System.out.println(a > b); // false
+		System.out.println(a + b <= c); // true
+		System.out.println(a + b >= c); // true
+	}
+
+}
