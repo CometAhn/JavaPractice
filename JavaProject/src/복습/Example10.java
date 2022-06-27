9c4bf32a276ad8ed9259cca871664378b680b217
CometAhn <comet@ipi.pw>
2022-06-27 14:35:38 +0900

    update

--- JavaProject/src/복습/Example10.java
+++ JavaProject/src/복습/Example10.java
@@ -22,22 +22,38 @@ public class Example10 {
 		 */
 
 		// for문을 이용하여 min부터 max까지 입력받은 수 만큼의 합을 구하고자 한다.
+		/*
+		 * Scanner input = new Scanner(System.in);
+		 * 
+		 * System.out.print("범위의 최소 값을 입력하세요. : "); int min = input.nextInt();
+		 * System.out.print("범위의 최대 값을 입력하세요. : "); int max = input.nextInt();
+		 * 
+		 * int sum = 0;
+		 * 
+		 * for (int i = min; i <= max; i++) { if (i % 2 == 1) { sum += i; } }
+		 * 
+		 * System.out.printf("%d~%d 사이의 모든 홀수 값의 합 : %d", min, max, sum);
+		 */
+
+		// for문을 이용 10명의 점수를 입력받아 10명의 총합과 평균값을 구하는 프로그램을 작성하시오.
+		// 일단 배열없이
+		// int[] student = new int[10];
+
 		Scanner input = new Scanner(System.in);
 
-		System.out.print("범위의 최소 값을 입력하세요. : ");
-		int min = input.nextInt();
-		System.out.print("범위의 최대 값을 입력하세요. : ");
-		int max = input.nextInt();
-
 		int sum = 0;
+		int aver = 0;
+		int number = 0;
 
-		for (int i = min; i <= max; i++) {
-			if (i % 2 == 1) {
-				sum += i;
-			}
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf("%d번 학생의 점수는? : ", i);
+			int ans = input.nextInt();
+
+			sum += ans;
+			number++;
 		}
-		System.out.printf("%d~%d 사이의 모든 홀수 값의 합 : %d", min, max, sum);
-
+		System.out.printf("10명의 점수 총합은 : %d\n", sum);
+		System.out.printf("10명의 점수 평균은 : %.1f", (float) sum / number);
 	}
 
 }
