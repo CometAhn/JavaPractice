99497106136c68822a911028fdd65ae519e1e459
CometAhn <comet@ipi.pw>
2022-06-27 17:43:33 +0900

    update

---
+++ JavaProject/src/복습/간단_코드_연습.java
@@ -0,0 +1,46 @@
+package 복습;
+
+public class 간단_코드_연습 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 간단 코드 연습
+		// 1. 다음 코드를 for문을 사용하여 다시 만들어 보시오.
+
+		// int i = 1;
+		// while (i <= 20) {
+		// i = i + 2;
+		// }
+
+		// for (int i = 1; i <= 20; i++) {
+		// i += 2;
+		// }
+
+		// 2. 다음 코드를 do 문을 사용하여 다시 만들어 보시오.
+
+		// int i = 1;
+		// while (i <= 10) {
+		// System.out.print("한 번 더");
+		// i = i + 2;
+		// }
+
+		// int i = 1;
+		// do {
+		// System.out.print("한 번 더");
+		// i = i + 2;
+		// } while (i <= 10);
+
+		// 3. 다음 코드를 for문을 사용하여 다시 만들어 보시오.
+		// int i = 1;
+		// while(i<10) {
+		// i++;
+		// System.out.print(i);
+		// }
+
+		// for(int i =1; i< 10; i++) {
+		// System.out.print(i);
+		// }
+	}
+
+}
