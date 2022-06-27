e03ec739bccb4cfb2823ab2ce20976c5b152b697
CometAhn <comet@ipi.pw>
2022-06-27 17:51:59 +0900

    update

--- JavaProject/src/복습/간단_코드_연습.java
+++ JavaProject/src/복습/간단_코드_연습.java
@@ -28,7 +28,7 @@ public class 간단_코드_연습 {
 		// int i = 1;
 		// do {
 		// System.out.print("한 번 더");
-		// i = i + 2;
+		// i += 2;
 		// } while (i <= 10);
 
 		// 3. 다음 코드를 for문을 사용하여 다시 만들어 보시오.
