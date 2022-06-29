0418bd742a28a082e719d263308b898763992bfe
CometAhn <comet@ipi.pw>
2022-06-29 17:04:30 +0900

    update

--- JavaProject/src/Class/Class.java
+++ JavaProject/src/Class/Class.java
@@ -1,11 +1,35 @@
-package Class; // 없을 시 왜 오류?
+package Class;
 
 class sub {
-	int result;
+	int result; // 속성(변수)
 
-	int calculator(int a, int b) {
+	int calculator(int a, int b) { // calculator :메서드, 함수(기능)
 		result = a + b;
 		return result;
+	}
+}
+
+class Car {
+	boolean powerOn;
+	String color;
+	int wheel;
+	int speed;
+	boolean wiperOn;
+
+	void power() {
+		powerOn = !powerOn;
+	}
+
+	void speedUp() {
+		speed++;
+	}
+
+	void speedDown() {
+		speed--;
+	}
+
+	void wiper() {
+		wiperOn = !wiperOn;
 	}
 }
 
@@ -15,7 +39,22 @@ public class Class {
 		// TODO Auto-generated method stub
 		sub sb = new sub();
 		int sum = sb.calculator(100, 200);
-		System.out.print(sum);
+		System.out.println(sum);
+
+		Car mycar = new Car();
+
+		System.out.println("시동 처음 초기화 : " + mycar.powerOn);
+		System.out.println("차의 색상 초기화 : " + mycar.color);
+		System.out.println("바퀴의 수 초기화 : " + mycar.wheel);
+		System.out.println("속력 초기화 : " + mycar.speed);
+		System.out.println("와이퍼 작동 초기화 : " + mycar.wiperOn);
+
+		mycar.power();
+		System.out.println("시동 켜짐? : " + mycar.powerOn);
+		mycar.power();
+		System.out.println("시동 켜짐? : " + mycar.powerOn);
+		mycar.color = "black";
+		System.out.println("차 색 : " + mycar.color);
 
 	}
 
