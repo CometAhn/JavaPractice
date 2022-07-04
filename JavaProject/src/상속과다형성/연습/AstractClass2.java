875dde478f370b990668cdbea6622ac51943bffc
CometAhn <comet@ipi.pw>
2022-07-04 16:37:00 +0900

    update

--- JavaProject/src/상속과다형성/연습/AstractClass2.java
+++ JavaProject/src/상속과다형성/연습/AstractClass2.java
@@ -2,21 +2,22 @@ package 상속과다형성.연습;
 
 abstract class animal1 {
 
-	String howling;
-	String howling1;
 	String name;
 	int age;
+	String howling;
+	String howling1;
 
 	animal1(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
 
+	void howl() {
+		System.out.println(this.howling + " 내 이름은 " + this.name + ", " + this.age + "살이다" + this.howling1);
+	}
+
 	abstract void charming(); // 공통적으로 들어가는 건 추상화 하지 않음.
 
-	void howl() {
-		System.out.println(this.howling + " 내 이름은 " + this.name + "," + this.age + "살이다" + this.howling1);
-	}
 }
 
 class Cat extends animal1 {
@@ -52,16 +53,22 @@ class Dog extends animal1 {
 }
 
 public class AstractClass2 {
+	public static void func(animal1 animal) {// b-type
+		animal.howl();
+		animal.charming();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		animal1 cat = new Cat("나비", 5);
 		animal1 dog = new Dog("바둑이", 3);
-		cat.howl();
-		cat.charming();
-		dog.howl();
-		dog.charming();
+		// cat.howl(); // a-type
+		// cat.charming();
+		// dog.howl();
+		// dog.charming();
+		func(cat); // b-type
+		func(dog);
 
 	}
 
