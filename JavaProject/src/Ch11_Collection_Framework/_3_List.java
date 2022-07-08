435631c3e318e25a8830ef217518150fcb8d8ab2
CometAhn <comet@ipi.pw>
2022-07-08 15:14:51 +0900

    List add

---
+++ JavaProject/src/Ch11_Collection_Framework/_3_List.java
@@ -0,0 +1,28 @@
+package Ch11_Collection_Framework;
+
+import java.util.ArrayList;
+
+public class _3_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList list1 = new ArrayList(10);
+		list1.add("A");
+		list1.add("C");
+		list1.add("E");
+		list1.add("D");
+		System.out.print("초기상태 : ");
+		System.out.println(list1);
+
+		System.out.println("인덱스 1 위치에 B추가");
+		list1.add(1, "B");
+		System.out.println(list1);
+
+		System.out.println("인덱스 2 위치의 값 삭제");
+		list1.remove(2);
+		System.out.println(list1);
+
+		System.out.println("인덱스 2번 위치의 값 반환 : " + list1.get(2));
+	}
+
+}
