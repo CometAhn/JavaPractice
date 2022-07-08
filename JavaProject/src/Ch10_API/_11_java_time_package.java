3fc9d76bdac104918fc66a1bd01f7ee752b4e8fa
CometAhn <comet@ipi.pw>
2022-07-08 12:27:11 +0900

    TemporlAdjuster add

--- JavaProject/src/Ch10_API/_11_java_time_package.java
+++ JavaProject/src/Ch10_API/_11_java_time_package.java
@@ -1,8 +1,10 @@
 package Ch10_API;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
 
 public class _11_java_time_package {
 
@@ -69,6 +71,38 @@ public class _11_java_time_package {
 		}
 		System.out.println("1900년부터 2100년까지 윤년은 총" + count + "번 있습니다.");
 		// LeapYear Start End
+		System.out.println();
+
+		// TemporlAdjuster Start
+		LocalDateTime ldt = LocalDateTime.now();
+		System.out.println(ldt);
+		LocalDateTime new_ldt;
+
+		new_ldt = ldt.with(TemporalAdjusters.firstDayOfYear());
+		System.out.println("올해 첫 번째 날 : " + new_ldt);
+		new_ldt = ldt.with(TemporalAdjusters.lastDayOfYear());
+		System.out.println("올해 마지막 날 : " + new_ldt);
+
+		new_ldt = ldt.with(TemporalAdjusters.firstDayOfMonth());
+		System.out.println("이번 달의 첫번째 날 :" + new_ldt);
+		new_ldt = ldt.with(TemporalAdjusters.lastDayOfMonth());
+		System.out.println("이번 달의 마지막 날 :" + new_ldt);
+
+		new_ldt = ldt.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+		System.out.println("이번 달의 첫 월요일 :" + new_ldt);
+		new_ldt = ldt.with(TemporalAdjusters.lastInMonth(DayOfWeek.SUNDAY));
+		System.out.println("이번 달의 마지막 일요일 :" + new_ldt);
+
+		new_ldt = ldt.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+		System.out.println("돌아오는 금요일 :" + new_ldt);
+
+		new_ldt = ldt.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
+		System.out.println("오늘을 포함한 다음 금요일 :" + new_ldt);
+		new_ldt = ldt.with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+		System.out.println("지난 월요일 :" + new_ldt);
+		new_ldt = ldt.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+		System.out.println("오늘을 포함한 지난 월요일 :" + new_ldt);
+		// TemporlAdjuster End
 
 	}
 
