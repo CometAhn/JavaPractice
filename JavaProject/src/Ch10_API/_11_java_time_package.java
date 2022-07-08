d35961267ca9f5e2511979cf1e29beb3040523f4
CometAhn <comet@ipi.pw>
2022-07-08 11:30:04 +0900

    LocalDate, LocalDateTime add

---
+++ JavaProject/src/Ch10_API/_11_java_time_package.java
@@ -0,0 +1,39 @@
+package Ch10_API;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class _11_java_time_package {
+
+	public static void main(String[] args) {
+
+		// LocalDate Start
+		LocalDate Id = LocalDate.now();
+		System.out.println(Id);
+
+		LocalDate Id2 = Id.minusYears(2).plusMonths(3).minusDays(4);
+		System.out.println(Id2);
+
+		// 자동변환
+		LocalDate Id3 = Id2.minusDays(3);
+		System.out.println(Id3);
+
+		// 주 더하기
+		LocalDate Id4 = Id3.plusWeeks(3);
+		System.out.println(Id4);
+		// LocalDate End
+		System.out.println();
+
+		// LocalDateTime Start
+		LocalDateTime Id5 = LocalDateTime.now();
+		System.out.println("현재 시간 : " + Id5);
+		LocalDateTime Id6 = Id5.minusHours(5).plusMinutes(30).minusSeconds(4);
+
+		System.out.println("변경 시간 : " + Id6);
+
+		LocalDateTime Id7 = Id6.minusHours(24);
+		System.out.println("자동 변환 시간 : " + Id7);
+		// LocalDateTime End
+	}
+
+}
