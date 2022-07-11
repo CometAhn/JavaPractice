c3b2114454dcc8652a8b99d3a933c71c1d30e555
CometAhn <comet@ipi.pw>
2022-07-11 15:23:00 +0900

    edit

--- JavaProject/src/Ch12_Thread/_1_1_test.java
+++ JavaProject/src/Ch12_Thread/_1_1_test.java
@@ -2,7 +2,7 @@ package Ch12_Thread;
 
 public class _1_1_test {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 
 		_1_2_Go g = new _1_2_Go();
 		_1_3_Come c = new _1_3_Come();
