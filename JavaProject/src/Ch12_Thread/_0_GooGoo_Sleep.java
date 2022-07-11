369932cd9f774a1beca3941dbdeeef861451300e
CometAhn <comet@ipi.pw>
2022-07-11 17:34:00 +0900

    edit String

--- JavaProject/src/Ch12_Thread/_0_GooGoo_Sleep.java
+++ JavaProject/src/Ch12_Thread/_0_GooGoo_Sleep.java
@@ -11,14 +11,14 @@ public class _0_GooGoo_Sleep implements Runnable {
 	public void run() {
 		long sleepTime = (long) (Math.random() * 500);
 		System.out.println(dan + "단이" + sleepTime + "만큼 sleep...");
-		try {
+		try { // 스레드 구동시 추가적으로 문제가 없는지 확인
 			Thread.sleep(sleepTime);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		for (int i = 1; i <= 9; i++) {
-			System.out.println(dan + "단 : " + dan + " * " + i + "=" + dan * i);
+			System.out.println(dan + "단 : " + dan + " * " + i + " = " + dan * i);
 		}
 	}
 
