f221d5c64fc5dfefaa2de677504a9f8f06300429
CometAhn <comet@ipi.pw>
2022-07-11 14:56:21 +0900

    Thread 설명 추가

--- JavaProject/src/Ch12_Thread/Thread_Test.java
+++ JavaProject/src/Ch12_Thread/Thread_Test.java
@@ -11,3 +11,17 @@ public class Thread_Test {
 	}
 
 }
+//Thread 생성하는 방법
+//
+//1. java.lang.Thread 클래스를 이용하는 방법.
+//	thread 클래스를 상속 받는 클래스를 작성한다.
+//	public void rum() 메소드를 오버라이딩 한다.
+//	main() 메소드에서 Thread 클래스를 상속하는 클래스의 객체를 생성한다.
+//	해당 객체의 start() 메소드를 호출하여 스레드를 실행 시킨다.
+//
+//2. java.lang.Runnable 인터페이스를 이용하는 방법.
+//	Runnable 인터페이스를 구성하는 클래스를 작성한다.
+//	public void run() 메소드를 오버라이딩한다.
+//	main() 메소드에서 Runnable 인터페이스를 궇ㄴ하는 클래스의 객체를 생성한다.
+//	Thread 클래스의 객체를 생성하면서 생성자의 인자로 Runnable 인터페이스를 구현한 클래스의 객체를 전달한다.
+//	Thread 객체의 start() 메소드를 호출한다.
