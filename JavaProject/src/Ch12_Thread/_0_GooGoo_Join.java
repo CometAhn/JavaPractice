c545c83911489c3ae8846a5bb5fb191aa3b44db9
CometAhn <comet@ipi.pw>
2022-07-11 18:01:05 +0900

    add file

---
+++ JavaProject/src/Ch12_Thread/_0_GooGoo_Join.java
@@ -0,0 +1,21 @@
+package Ch12_Thread;
+
+public class _0_GooGoo_Join implements Runnable {
+	private int dan;
+
+	public _0_GooGoo_Join(int dan) {
+		this.dan = dan;
+	}
+
+	public void run() {
+		try {
+			Thread.sleep(500);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(dan + "단 : " + dan + " * " + i + "=" + dan * i);
+		}
+	}
+}
