1d4171686f6573bc4a3b4f416a1d3a03634d2478
CometAhn <comet@ipi.pw>
2022-07-12 17:20:33 +0900

    change file name

---
+++ JavaProject/src/Ch06_Class_Review/_02_0_Sum_Method_main.java
@@ -0,0 +1,10 @@
+package Ch06_Class_Review;
+
+public class _02_0_Sum_Method_main {
+
+	public static void main(String[] args) {
+		_02_1_Sum_Method a = new _02_1_Sum_Method();
+		a.sum();
+	}
+
+}
--- JavaProject/src/Ch06_Class_Review/_02_1_Sum_Method.java
+++ JavaProject/src/Ch06_Class_Review/_02_1_Sum_Method.java
@@ -2,26 +2,18 @@ package Ch06_Class_Review;
 
 import java.util.Scanner;
 
-class sum {
-
+public class _02_1_Sum_Method {
 	public void sum() {
 
 		Scanner sc = new Scanner(System.in);
-
+		System.out.println("a, b의 값을 입력 받아 두 값의 합을 구하고자 한다.");
+		System.out.println("a의 값은?");
 		int a = sc.nextInt();
+		System.out.println("b의 값은?");
 		int b = sc.nextInt();
 
 		int sum = a + b;
 
 		System.out.println(a + " + " + b + " = " + sum);
 	}
-}
-
-public class _02_Sum_Method {
-
-	public static void main(String[] args) {
-		sum a = new sum();
-		a.sum();
-	}
-
 }
