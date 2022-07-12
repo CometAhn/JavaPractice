90bb60361b9bfd4a9a4635d97188801dee7ea031
CometAhn <comet@ipi.pw>
2022-07-12 15:36:26 +0900

    ?

--- JavaProject/src/Ch13_IO/_03_File_IO_Test.java
+++ JavaProject/src/Ch13_IO/_03_File_IO_Test.java
@@ -1,10 +1,31 @@
 package Ch13_IO;
 
+import java.io.InputStream;
+
 public class _03_File_IO_Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		InputStream is = null;
+		try {
+			byte[] b = new byte[5];
+			System.out.println("데이터를 입력하세요. :");
+			is = System.in;
+			int n = is.read(b);
+			System.out.println("입력 개수 : " + n);
+			for (byte bb : b) {
+				System.out.println("입력 값 : " + ((char) bb));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
