76c6e91fe2211aa96e56744ba041ef473d27ebd9
CometAhn <comet@ipi.pw>
2022-07-13 09:52:38 +0900

    add Frame

--- JavaProject/src/Ch14_GUI/_01_Swing_Frame.java
+++ JavaProject/src/Ch14_GUI/_01_Swing_Frame.java
@@ -1,9 +1,26 @@
 package Ch14_GUI;
-import java.awt.*;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+
+class AddFrame extends JFrame {
+	public AddFrame() {
+		setTitle("Title");
+		setSize(300, 300);
+		setVisible(true);
+
+		this.setLayout(new FlowLayout());
+
+		// x 버튼 클릭 시 종료
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
 public class _01_Swing_Frame {
 
 	public static void main(String[] args) {
-
+		AddFrame a = new AddFrame();
 	}
 
 }
