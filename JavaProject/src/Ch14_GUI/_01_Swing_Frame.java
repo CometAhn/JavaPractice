7ae70f9887004a514c87532e69e683725450155b
CometAhn <comet@ipi.pw>
2022-07-13 10:06:51 +0900

    Add Component

--- JavaProject/src/Ch14_GUI/_01_Swing_Frame.java
+++ JavaProject/src/Ch14_GUI/_01_Swing_Frame.java
@@ -2,15 +2,30 @@ package Ch14_GUI;
 
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 
 class AddFrame extends JFrame {
 	public AddFrame() {
 		setTitle("Title");
 		setSize(300, 300);
-		setVisible(true);
 
+		// 컴포넌트 객체 생성
+		JButton button = new JButton("Button");
+		JCheckBox box = new JCheckBox("체크박스");
+		JSlider slide = new JSlider();
+		JTextField tf = new JTextField("Text입력", 20);
 		this.setLayout(new FlowLayout());
+
+		// 컴포넌트 객체 추가.
+		this.add(button);
+		this.add(box);
+		this.add(slide);
+		this.add(tf);
+		setVisible(true);
 
 		// x 버튼 클릭 시 종료
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
