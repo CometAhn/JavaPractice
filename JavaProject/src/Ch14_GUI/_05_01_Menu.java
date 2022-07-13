f4dae59c9c3c29ed3e9761365f0dffcb5a1fd20d
CometAhn <comet@ipi.pw>
2022-07-13 14:19:10 +0900

    MenuBar

--- JavaProject/src/Ch14_GUI/_05_01_Menu.java
+++ JavaProject/src/Ch14_GUI/_05_01_Menu.java
@@ -5,10 +5,35 @@
  */
 package Ch14_GUI;
 
-public class _05_01_Menu {
+import javax.swing.*;
+
+public class _05_01_Menu extends JFrame {
+	_05_01_Menu() {
+		setTitle("Menu 만들어보기");
+		createMenu();
+		setSize(250, 200);
+		setVisible(true);
+	}
+
+	void createMenu() {
+		JMenuBar mb = new JMenuBar();
+		JMenu screenMenu = new JMenu("메뉴바 1");
+
+		screenMenu.add(new JMenuItem("Load"));
+		screenMenu.add(new JMenuItem("Hide"));
+		screenMenu.add(new JMenuItem("ReShow"));
+		screenMenu.addSeparator();
+		screenMenu.add(new JMenuItem("EXit"));
+
+		mb.add(screenMenu);
+		mb.add(new JMenu("편집"));
+		mb.add(new JMenu("실행"));
+		mb.add(new JMenu("도움말"));
+		setJMenuBar(mb);
+	}
 
 	public static void main(String[] args) {
-
+		new _05_01_Menu();
 	}
 
 }
