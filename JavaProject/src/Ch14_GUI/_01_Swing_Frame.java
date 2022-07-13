61981dade8401b0145203f348a8af36a30ea9ac2
CometAhn <comet@ipi.pw>
2022-07-13 10:25:58 +0900

    주석 추가

--- JavaProject/src/Ch14_GUI/_01_Swing_Frame.java
+++ JavaProject/src/Ch14_GUI/_01_Swing_Frame.java
@@ -6,22 +6,23 @@ import javax.swing.*;
 
 class AddFrame extends JFrame {
 	public AddFrame() {
-		setTitle("Title");
-		setSize(300, 300);
+		setTitle("Title"); // 타이틀 이름
+		setSize(300, 300); // 프레임 사이즈
+		setLocation(50, 50); // 창을 좌측 상단으로부터 x, y 만큼 띄운다.
 
 		// 컴포넌트 객체 생성
-		JButton button = new JButton("Button");
-		JCheckBox box = new JCheckBox("체크박스");
-		JSlider slide = new JSlider();
-		JTextField tf = new JTextField("Text입력", 20);
-		this.setLayout(new FlowLayout());
+		JButton button = new JButton("Button"); // Button이라는 이름의 버튼
+		JCheckBox box = new JCheckBox("체크박스"); // 체크박스라는 이름의 체크박스
+		JSlider slide = new JSlider(); // slide라는 이름의 슬라이드
+		JTextField tf = new JTextField("Text입력", 20); // tf라는 text입력이 입력된 길이 20의 텍스트 필드
+		this.setLayout(new FlowLayout()); // 컴포넌트 보이게. 없을 경우 text필드는 필드는 안 보이고 텍스트만 보임.
 
 		// 컴포넌트 객체 추가.
 		this.add(button);
 		this.add(box);
 		this.add(slide);
 		this.add(tf);
-		setVisible(true);
+		setVisible(true); // 없을 시 프레임이 안 보임.
 
 		// x 버튼 클릭 시 종료
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
