e1a6fc772e9880817db3ea1e02ad0ce9ddc094c0
CometAhn <comet@ipi.pw>
2022-07-14 17:46:10 +0900

    add improved for

--- JavaProject/src/_review/_20220714_Array.java
+++ JavaProject/src/_review/_20220714_Array.java
@@ -13,7 +13,15 @@ public class _20220714_Array {
 		for (int i = 0; i < num.length; i++) {
 			System.out.println("배열 값 : " + num[i]);
 		}
-
+		/**
+		 * 방법 1 for (int i = 0; i < num.length; i++)
+		 */
+		for (int i : num) {
+			System.out.println("배열 값 : " + i);
+		}
+		/**
+		 * 방법 2 for (int i : num)
+		 */
 	}
 
 }
