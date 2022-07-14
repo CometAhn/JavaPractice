5d0e95662cc731a1e8e49ad4fe539bcbfe55fc16
CometAhn <comet@ipi.pw>
2022-07-14 17:34:03 +0900

    add Arrays exam

---
+++ JavaProject/src/_review/_20220714_Array.java
@@ -0,0 +1,19 @@
+package _review;
+
+public class _20220714_Array {
+
+	public static void main(String[] args) {
+
+		int[] num;
+		num = new int[3];
+
+		num[0] = 1;
+		num[1] = 2;
+		num[2] = 3;
+		for (int i = 0; i < num.length; i++) {
+			System.out.println("배열 값 : " + num[i]);
+		}
+
+	}
+
+}
