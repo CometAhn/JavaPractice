5c90df8b65ae6b95ceef1865d14b14de125bb3d3
CometAhn <comet@ipi.pw>
2022-07-18 10:39:30 +0900

    Add get IpAdrress

--- JavaProject/src/Ch15_Network_Programming/_01_classIP.java
+++ JavaProject/src/Ch15_Network_Programming/_01_classIP.java
@@ -9,7 +9,7 @@ public class _01_classIP {
 		InetAddress ip = null;
 
 		try {
-			ip = InetAddress.getLocalHost();
+			ip = InetAddress.getByName("www.daum.net");
 
 			System.out.println(ip.getHostName());
 
@@ -17,7 +17,45 @@ public class _01_classIP {
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+		try {
+			ip = InetAddress.getByName("www.naver.com");
 
+			System.out.println(ip.getHostName());
+
+			System.out.println(ip.getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		try {
+			ip = InetAddress.getByName("google.com");
+
+			System.out.println(ip.getHostName());
+
+			System.out.println(ip.getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		try {
+			ip = InetAddress.getByName("https://eieifemiofmeowfmwe.com/"); // 오류
+
+			System.out.println(ip.getHostName());
+
+			System.out.println(ip.getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.out.println("오류");
+		}
+		/*
+			try {
+				ip = InetAddress.getLocalHost();
+		
+				System.out.println(ip.getHostName());
+		
+				System.out.println(ip.getHostAddress());
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+		*/
 	}
 
 }
