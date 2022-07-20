227263db974f51fe82304bf434e79b08914f6621
CometAhn <comet@ipi.pw>
2022-07-20 16:12:49 +0900

    뭐했지 내가

--- JavaProject/src/Ch00_MySQL_DB_Connection/DBSQL_UPDATE.java
+++ JavaProject/src/Ch00_MySQL_DB_Connection/DBSQL_UPDATE.java
@@ -3,9 +3,9 @@ package Ch00_MySQL_DB_Connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.PreparedStatement; // 파라미터를 이용한 데이터 전달
+import java.sql.PreparedStatement;
 
-public class DBSQL_INSERT {
+public class DBSQL_UPDATE {
 
 	public static void main(String[] args) {
 		Connection con = null;
@@ -17,7 +17,7 @@ public class DBSQL_INSERT {
 		String user = "root";
 		String pw = "0000";
 
-		String SQL = "insert into hongong1(toy_id, toy_name, age) values(?, ?, ?)";
+		String SQL = "update hongong1 set toy_name=?, age=? where toy_id=?";
 
 		try {
 			Class.forName(driver);
@@ -25,9 +25,10 @@ public class DBSQL_INSERT {
 			con = DriverManager.getConnection(url, user, pw);
 
 			pstmt = con.prepareStatement(SQL);
-			pstmt.setInt(1, 4);
-			pstmt.setString(2, "루이");
-			pstmt.setInt(3, 30);
+
+			pstmt.setString(1, "루이2");
+			pstmt.setInt(2, 28);
+			pstmt.setInt(3, 4);
 
 			int r = pstmt.executeUpdate();
 
